4acf0c248c7a31d01258c6b84b738e9ed4b79f30
Loumin1014 <31617645+Loumin1014@users.noreply.github.com>
2018-11-02 16:12:11 +0100

    Add files via upload

--- echauffement_3/login.js
+++ echauffement_3/login.js
@@ -2,13 +2,14 @@ var demandeEmail = prompt("Votre adresse email ?");
 var posAt = demandeEmail.indexOf("@"); // position du @
 
 
-if (posAt != -1) {
+if (posAt != -1 && demandeEmail.length > 4) {
     // si posAt n'est pas égal à -1
+    //si le string contient au moin 4 caracteres
     // si le symbole "@" n'est pas trouvé dans la chaîne de caractères, posAt est égal à -1
     alert("L'adresse email semble correcte.");
 } else {
     //etape 1 alert("L'adresse email ne contient pas d'@');
-    demandeEmail = prompt("Merci d'entrer une adresse email valide ! ");
+    demandeEmail = prompt("Merci d'entrer une adresse email valide Qui comporte au moin 4 caracteres ! ");
 
 }
 var mdp = prompt("Entrez votre mot de passe :");
